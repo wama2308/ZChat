a9551b1d73a90669a13c3b39eab50c2ab58101f9
wama2308 <wilfredomedina11@gmail.com>
2025-04-07 17:36:47 -0400

    instalacion de husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+  printWidth: 100,
 };
